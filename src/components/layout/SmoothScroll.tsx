b3127a6edf7480d0bb7735416d2e13d82749e1b4
Malik Asad <malik.asad.ayub.awan@gmail.com>
2026-08-06 07:27:25 +0500

    Improved scroll speed

--- src/components/layout/SmoothScroll.tsx
+++ src/components/layout/SmoothScroll.tsx
@@ -12,7 +12,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
     if (mql.matches) return;
 
     const lenis = new Lenis({
-      duration: 1.15,
+      duration: 0.5,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     });
